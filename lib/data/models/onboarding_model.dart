0722ab5e6d640ccfbe3d94e5f86637e2be6d9566
mahaelasil <134947311+mahaelasil@users.noreply.github.com>
2023-07-13 01:24:50 +0300

    counter

---
+++ lib/data/models/onboarding_model.dart
@@ -0,0 +1,10 @@
+class OnBoardingModel {
+  late final String image;
+  late final String title;
+  late final String body;
+  OnBoardingModel({
+    required this.image,
+    required this.body,
+    required this.title,
+  });
+}
